9610ddd0da218bdd240d58315d392e54ef354e87
borjasper-sys <borjasper@hotmail.com>
2026-05-25 11:20:59 +0200

    Seed Master user via migration

---
+++ prisma/migrations/20260525094500_seed_master_user/migration.sql
@@ -0,0 +1,23 @@
+INSERT INTO "User" (
+    "id",
+    "name",
+    "email",
+    "passwordHash",
+    "role",
+    "active",
+    "createdAt"
+)
+VALUES (
+    'master-user',
+    'Master',
+    'Master',
+    '$2b$12$EGQeRbalGXvsu7TqHafOdOxBgopMmIRjEujf5BfMxLzFWzg9DT/6O',
+    'MASTER',
+    true,
+    CURRENT_TIMESTAMP
+)
+ON CONFLICT("email") DO UPDATE SET
+    "name" = 'Master',
+    "passwordHash" = '$2b$12$EGQeRbalGXvsu7TqHafOdOxBgopMmIRjEujf5BfMxLzFWzg9DT/6O',
+    "role" = 'MASTER',
+    "active" = true;
